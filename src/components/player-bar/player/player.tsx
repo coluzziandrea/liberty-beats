cdc6684e9b269ac69067ede87615d5cc8f1ef546
Coluzzi Andrea <coluzziandrea.dev@gmail.com>
2024-03-02 01:07:36 +0100

    hello tonejs

--- src/components/player-bar/player/player.tsx
+++ src/components/player-bar/player/player.tsx
@@ -1,10 +1,22 @@
 import { component$ } from "@builder.io/qwik";
 
+import * as Tone from "tone";
+
 export const Player = component$(() => {
   return (
     <div>
       <button>{"|<"}</button>
-      <button>{">"}</button>
+      <button
+        onClick$={() => {
+          //create a synth and connect it to the main output (your speakers)
+          const synth = new Tone.Synth().toDestination();
+
+          //play a middle 'C' for the duration of an 8th note
+          synth.triggerAttackRelease("C4", "8n");
+        }}
+      >
+        {">"}
+      </button>
       {/* <button>{"||"}</button> */}
       <button>{"[]"}</button>
       <span>{"00:00.0"}</span>
