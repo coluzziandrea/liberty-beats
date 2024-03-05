b41c8479af20a50708c8eb2b5ffd5c9c776d5916
Coluzzi Andrea <coluzziandrea.dev@gmail.com>
2024-03-05 12:58:23 +0100

    add track will select it

--- src/features/daw/playlist-header/playlist-commands/playlist-commands.tsx
+++ src/features/daw/playlist-header/playlist-commands/playlist-commands.tsx
@@ -1,36 +1,34 @@
 import { useDispatch } from 'react-redux'
-import { addTrack } from '../../playlist/store/playlist-slice'
+import { addTrack, selectTrack } from '../../playlist/store/playlist-slice'
 import { TRACK_COLORS } from '../../../../model/track/track-color'
+import { Track } from '../../../../model/track/track'
 
 export const PlaylistCommands = () => {
   const dispatch = useDispatch()
 
+  const handleAddTrack = () => {
+    const newTrack: Track = {
+      id: Date.now().toString(),
+      title: 'New Track',
+      color: TRACK_COLORS[Math.floor(Math.random() * TRACK_COLORS.length)],
+      instrumentType: 'PIANO',
+      bars: [
+        {
+          id: Date.now().toString(),
+          title: 'New Bar',
+          startAtTick: 0,
+          endAtTick: 10,
+          notes: [],
+        },
+      ],
+    }
+    dispatch(addTrack(newTrack))
+    dispatch(selectTrack(newTrack))
+  }
+
   return (
     <div className="flex flex-row">
-      <button
-        onClick={() =>
-          dispatch(
-            addTrack({
-              id: Date.now().toString(),
-              title: 'New Track',
-              color:
-                TRACK_COLORS[Math.floor(Math.random() * TRACK_COLORS.length)],
-              instrumentType: 'PIANO',
-              bars: [
-                {
-                  id: Date.now().toString(),
-                  title: 'New Bar',
-                  startAtTick: 0,
-                  endAtTick: 10,
-                  notes: [],
-                },
-              ],
-            })
-          )
-        }
-      >
-        {'+ Add Track'}
-      </button>
+      <button onClick={handleAddTrack}>{'+ Add Track'}</button>
     </div>
   )
 }
